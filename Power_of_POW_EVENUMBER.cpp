a1c90a9c9413b92915a6a1ceea82792b53d9076b
spindyzel <husniramdani18@gmail.com>
2018-09-30 20:58:28 +0700

    ganti jawaban

--- Power_of_POW_EVENUMBER.cpp
+++ Power_of_POW_EVENUMBER.cpp
@@ -1,21 +1,18 @@
 // https://practice.geeksforgeeks.org/problems/power-of-pow-even-number/0/?ref=self
 #include<bits/stdc++.h>
 using namespace std;
-int t,n,arr[100],k,num;
+int t,n;
 
 int main(){
     cin >> t;
-    for(int j=0; j<t; j++){
+    while(t--){
         cin >> n;
         int m = -1;
         int jumlah = 0;
         for(int i=0; i<n; i++){
             m += 2;
-            jumlah += m*m;
+            jumlah += pow(m,2);
         }
-        arr[j] = jumlah;
-    }
-    for(int i=0; i<t; i++){
-        cout << arr[i] << endl;
+        cout << jumlah << endl;
     }
 }
